1297d9588627d30ff5b9b60f9632c09653da7aeb
NatanRibeiro <natan_r.dutra@hotmail.com>
2014-11-17 23:28:31 -0200

    UniTest

--- CompFacil.LojaVirtual.UnitTest/UnitTest1.cs
+++ CompFacil.LojaVirtual.UnitTest/UnitTest1.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace CompFacil.LojaVirtual.UnitTest
 {
     [TestClass]
-    public class UnitTest1
+    public class CompFacilUnitTest
     {
         [TestMethod]
         public void TestMethod1()
