adc16df9c509c249755f856c5d99bc7e6e22e788
NatanRibeiro <natan_r.dutra@hotmail.com>
2014-11-20 21:17:40 -0200

    Atualização: Referenciando o Entity Framework

---
+++ CompFacil.LojaVirtual.Dominio/Repositório/EfDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CompFacil.LojaVirtual.Dominio.Entidades;
+
+namespace CompFacil.LojaVirtual.Dominio.Repositório
+{
+    public class EfDbContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+        }
+
+    }
+
+}
---
+++ CompFacil.LojaVirtual.Web/Controllers/ProdutosController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CompFacil.LojaVirtual.Dominio.Repositório;
+
+namespace CompFacil.LojaVirtual.Web.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private ProdutosRepositorio _repositorio;
+
+        // GET: Produto
+        public ActionResult Index()
+        {
+            _repositorio = new ProdutosRepositorio();
+            var produtos = _repositorio.Produtos.Take(10);
+
+            return View(produtos);
+        }
+    }
+}
