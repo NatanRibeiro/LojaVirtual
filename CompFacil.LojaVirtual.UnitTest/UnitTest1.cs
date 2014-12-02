49133f79572dcec1f2cc0e8ba5d5014fc3506ff1
NatanRibeiro <natan_r.dutra@hotmail.com>
2014-12-01 22:40:06 -0200

    Paginacao

--- CompFacil.LojaVirtual.UnitTest/UnitTest1.cs
+++ CompFacil.LojaVirtual.UnitTest/UnitTest1.cs
@@ -3,13 +3,29 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CompFacil.LojaVirtual.UnitTest
 {
-    [TestClass]
     public class CompFacilUnitTest
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
+        public void Take()
+        {  
+            //O Operador Take é usado para selecionar os primeiros n objetos de uma coleção
 
+            int[] numeros = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            var resultado = from num in numeros.Take(5) select num;
+
+            int[] teste = { 5, 4, 1, 3, 9};
+
+            CollectionAssert.AreEqual(resultado.ToArray(), teste);
+        }
+
+        [TestMethod]
+        public void Skip()    
+        {
+            //O Operador Skip é usado para ignorar os primeiros n objetos de uma coleção
+
+            int[] numeros = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            var resultado = from num in numeros.Take(5).Skipe(2) select num;
         }
     }
 }
