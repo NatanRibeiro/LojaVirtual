8e0a09229d205cec3b2505a97ed5faeddde90b3c
NatanRibeiro <natan_r.dutra@hotmail.com>
2015-03-18 23:07:00 -0300

    Atualização: Menu lateral e correção do UnitTest

--- CompFacil.LojaVirtual.Web/Controllers/VitrineController.cs
+++ CompFacil.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -21,7 +21,7 @@ namespace CompFacil.LojaVirtual.Web.Controllers
             ProdutosViewModel model = new ProdutosViewModel
             {
                 Produtos = _repositorio.Produtos
-                    .Where(p => p.Categoria == null || p.Categoria == categoria)
+                    .Where(p => categoria == null || p.Categoria == categoria)
                     .OrderBy(p => p.Descricao)
                     .Skip((pagina - 1)*ProdutosPorPagina)
                     .Take(ProdutosPorPagina),
