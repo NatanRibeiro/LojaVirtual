a1fa3f1d0c5791f243917b856d189a9d6abbb939
NatanRibeiro <natan_r.dutra@hotmail.com>
2015-03-22 21:36:55 -0300

    Atualização: Início a construção da View Carrinho

---
+++ CompFacil.LojaVirtual.Web/Controllers/CarrinhoController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CompFacil.LojaVirtual.Dominio.Entidades;
+using CompFacil.LojaVirtual.Dominio.Repositório;
+using CompFacil.LojaVirtual.Web.Models;
+
+namespace CompFacil.LojaVirtual.Web.Controllers
+{
+    public class CarrinhoController : Controller
+    {
+        #region Propriedades
+        private ProdutosRepositorio oRepositorio;
+        #endregion
+
+        public RedirectToRouteResult Adicionar(int _ProdutoID, string _ReturnUrl)
+        {
+            oRepositorio = new ProdutosRepositorio();
+            Produto oProduto = oRepositorio.Produtos.FirstOrDefault(p => p.ProdutoID == _ProdutoID);
+
+            if (oProduto != null)
+                Obter().AdicionarItem(oProduto, 1);
+
+            return RedirectToAction("Index", new { _ReturnUrl });
+        }
+        
+        private Carrinho Obter()
+        {
+            Carrinho oCarrinho = (Carrinho) Session["Carrinho"];
+
+            if (oCarrinho == null)
+            {
+                oCarrinho = new Carrinho();
+                Session["Carrinho"] = oCarrinho;
+            }
+
+            return oCarrinho;
+        }
+
+        public RedirectToRouteResult Remover(int _ProdutoID, string _ReturnUrl)
+        {
+            oRepositorio = new ProdutosRepositorio();
+            Produto oProduto = oRepositorio.Produtos.FirstOrDefault(p => p.ProdutoID == _ProdutoID);
+
+            if (oProduto != null)
+                Obter().RemoverItem(oProduto);
+
+            return RedirectToAction("Index", new { _ReturnUrl });
+        }
+
+        public ViewResult Index(string _ReturnUrl)
+        {
+            return View(new CarrinhoViewModel
+            {
+                Carrinho = Obter(),
+                ReturnUrl = _ReturnUrl
+            });
+        }
+    }
+}
---
+++ CompFacil.LojaVirtual.Web/Models/CarrinhoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CompFacil.LojaVirtual.Dominio.Entidades;
+
+namespace CompFacil.LojaVirtual.Web.Models
+{
+    public class CarrinhoViewModel
+    {
+        public Carrinho Carrinho { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
