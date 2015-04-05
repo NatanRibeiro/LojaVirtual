5b7432573f83c5c3088e37a270365af2fb187fff
NatanRibeiro <natan_r.dutra@hotmail.com>
2015-04-05 20:48:34 -0300

    Update: Creation of Admin Area

---
+++ CompFacil.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CompFacil.LojaVirtual.Web.Areas.Administrativo.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private ProdutosRepositorio _repositorio;
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
