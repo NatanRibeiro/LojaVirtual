90cf6c605e99f223f9a64f4aae734214cf62eba4
NatanRibeiro <natan_r.dutra@hotmail.com>
2015-05-24 20:54:22 -0300

    UPDATE: Including the Authentication at Login

---
+++ CompFacil.LojaVirtual.Dominio/Repositório/AdministradoresRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CompFacil.LojaVirtual.Dominio.Entidades;
+
+namespace CompFacil.LojaVirtual.Dominio.Repositório
+{
+    public class AdministradoresRepositorio
+    {
+        private readonly EfDbContext _context = new EfDbContext();
+
+        public Administrador ObterAdministrador(Administrador adminitrador)
+        {
+            return _context.Administradores.FirstOrDefault(a => a.Login == administrador.Login);
+        }
+    }
+}
